456361f1228e68cd43d92783d2a1e5f3ab06559b
Hanru Xu <xjhzwx@gmail.com>
2021-11-24 15:26:51 +1100

    init commit

---
+++ pages/admin/[slug].js
@@ -0,0 +1,7 @@
+export default function AdminPostPage({}) {
+  return (
+    <main>
+      <h1>edit post</h1>
+    </main>
+  );
+}
---
+++ pages/enter.js
@@ -0,0 +1,48 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import firebase from 'firebase/app'
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+export default function EnterPage(props) {
+  const user = null;
+  const username = null;
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.jpg"} /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
+}
